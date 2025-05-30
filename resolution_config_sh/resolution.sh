a4e579943bc4902b516f7cfae5cd42e51cc98660
harshascode <harshamesta0088@gmail.com>
2025-05-30 09:49:04 +0530

    resolution sh added in dotfiles

---
+++ resolution_config_sh/resolution.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# chmod +x resolution.sh
+# Define the new mode
+MODEDEF="1920x1080_60.00"
+MODELINE="173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync"
+OUTPUT="VGA-1"
+
+# Add the new mode if it doesn't exist
+if ! xrandr | grep -q "$MODEDEF"; then
+  echo "Adding new mode: $MODEDEF"
+  xrandr --newmode "$MODEDEF" $MODELINE
+  xrandr --addmode "$OUTPUT" "$MODEDEF"
+else
+  echo "Mode $MODEDEF already exists."
+fi
+
+# Set the output to the new mode
+echo "Setting output $OUTPUT to mode $MODEDEF"
+xrandr --output "$OUTPUT" --mode "$MODEDEF"
+
+echo "xrandr setup complete."
+echo "Wifi turned off complete."
+rfkill block all 
+eval "$(zoxide init bash)" ~/.bashrc
